2713edb94d3af5adda5bf8c78b5890cb00af9781
Shravani <shravani.b3@gmail.com>
2021-06-10 12:06:29 +1000

    file structure

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+const Header = () => {
+    return (
+        <div>
+            <Link to='/'>  
+                Home
+            </Link>  
+            <Link to='/'>  
+                About
+            </Link>  
+            
+        </div>
+    )
+}
+
+export default Header;
---
+++ src/components/Match.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Match = () => {
+    return (
+        <div>
+            <h1>This is match page</h1>
+        </div>
+    )
+}
+
+export default Match
